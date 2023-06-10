1e40164f8e41f5d6fefa898df025816cbbba5785
eme22 <48258734+eme22@users.noreply.github.com>
2023-06-10 00:24:13 -0500

    Return id for Medic

---
+++ src/main/java/com/eme22/hospitalwebserver/RestConfig.java
@@ -0,0 +1,16 @@
+package com.eme22.hospitalwebserver;
+
+import com.eme22.hospitalwebserver.model.Medic;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Component
+public class RestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Medic.class);
+    }
+}
