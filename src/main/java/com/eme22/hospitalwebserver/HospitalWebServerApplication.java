49aea260bd80848f944b9d48fc078a7e7c50508a
eme22 <48258734+eme22@users.noreply.github.com>
2023-06-24 00:35:54 -0500

    Edit properties

--- src/main/java/com/eme22/hospitalwebserver/HospitalWebServerApplication.java
+++ src/main/java/com/eme22/hospitalwebserver/HospitalWebServerApplication.java
@@ -19,7 +19,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
+@SpringBootApplication
 @EnableJpaRepositories("com.eme22.hospitalwebserver.*")
 @ComponentScan(basePackages = { "com.eme22.hospitalwebserver.*" })
 @EntityScan("com.eme22.hospitalwebserver.*")
